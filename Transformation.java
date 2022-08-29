182435d7674c6fdf1a535b0a283270833a93e2f3
MedMaache <112427995+MedMaache@users.noreply.github.com>
2022-08-29 21:08:31 +0100

    Update Transformation.java

--- Transformation.java
+++ Transformation.java
@@ -1,3 +1,12 @@
+/**
+ * An enum class that defines the
+ * transformations <b>ID</b> (identity),  
+ * <b>ROT</b> (rotation)
+ * <b>VSYM</b> (vertical symmetry).
+ * and <b>HSYM</b> (horiuzontal symmetry).
+ *
+ */
+
 public enum Transformation {
 	ID,
 	ROT,
